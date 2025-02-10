8e86553410bac40c0859bb309ccbaaa89369e205
Alex <askow2015@gmail.com>
2025-02-10 15:30:34 -0700

    Refactored @files to use more idiomatic and edge case handling solution with File::expand_path

--- cms.rb
+++ cms.rb
@@ -2,8 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubi'
 
+root = File.expand_path("..", __FILE__)
+
 get '/' do
-  @files = Dir.new('data').children.sort
-  
+  @files = Dir.glob(root + '/data/*').map { |path| File.basename(path) }
+
   erb :index
 end
