afa07c705b96f397efbad2850aec2b9147c029c8
Alex <askow2015@gmail.com>
2025-02-10 15:55:38 -0700

    Added content reading for each file on each webpage

--- cms.rb
+++ cms.rb
@@ -4,8 +4,22 @@ require 'tilt/erubi'
 
 root = File.expand_path("..", __FILE__)
 
+before do
+	@files = Dir.glob(root + '/data/*')
+end
+
 get '/' do
-  @files = Dir.glob(root + '/data/*').map { |path| File.basename(path) }
+	@files = @files.map { |path| File.basename(path) }	
 
   erb :index
+end
+
+get '/:file_name' do
+	file_name = params[:file_name]
+	file_path = root + '/data/' + file_name
+
+	@file = File.readlines(file_path)
+
+	headers['Content-Type'] = 'text/plain'
+	erb :file
 end
